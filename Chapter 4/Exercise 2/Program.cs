52222d5c8ea98e5bff869e9a2abd29f6da4f070b
Mika <mika.juhani.vaisanen@outlook.com>
2024-04-14 00:59:23 +0300

    An exercise application that uses a do-while-loop

---
+++ Chapter 4/Exercise 2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1, var2;
+            Console.WriteLine("Please enter two numbers:");
+            do
+            {
+                var1 = Convert.ToInt32(Console.ReadLine());
+                var2 = Convert.ToInt32(Console.ReadLine());
+                if (var1 > 10 && var2 > 10)
+                    Console.WriteLine("You need to enter another numbers!");
+            }
+            while (var1 > 10 && var2 > 10);
+
+            if (var1 > 10 || var2 > 10)
+                Console.WriteLine("One of the numbers is greater than 10.");
+            Console.ReadKey();
+        }
+    }
+}
