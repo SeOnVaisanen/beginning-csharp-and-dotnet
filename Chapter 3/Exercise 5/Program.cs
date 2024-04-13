27f79dbd4d7e6bff266da497cd68e251b59a0235
Mika <mika.juhani.vaisanen@outlook.com>
2024-04-13 14:25:50 +0300

    An exercise application that outputs the product of given numbers

---
+++ Chapter 3/Exercise 5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber, thirdNumber, fourthNumber;
+            Console.WriteLine("Enter the first number:");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number:");
+            thirdNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the fourth number:");
+            fourthNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"The product of the numbers is: {firstNumber * secondNumber * thirdNumber * fourthNumber}.");
+            Console.ReadKey();
+        }
+    }
+}
