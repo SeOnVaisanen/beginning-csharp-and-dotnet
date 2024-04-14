53b094983f6b14398d21e649d561e0839785d4f1
Mika <mika.juhani.vaisanen@outlook.com>
2024-04-14 13:49:45 +0300

    A console application that uses various type conversions

---
+++ Chapter 5/TypeConversions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TypeConversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short shortResult, shortVal = 4;
+            int integerVal = 67;
+            long longResult;
+            float floatVal = 10.5f;
+            double doubleResult, doubleVal = 99.999;
+            string stringResult, stringVal = "17";
+            bool boolVal = true;
+            Console.WriteLine("Variable Conversion Examples\n");
+            doubleResult = floatVal * shortVal;
+            Console.WriteLine($"Implicit, -> double: {floatVal} * {shortVal} ->" +
+                $"{doubleResult}");
+            shortResult = (short)floatVal;
+            Console.WriteLine($"Explicit, -> short: {floatVal} -> {shortResult}");
+            stringResult = Convert.ToString(boolVal) + Convert.ToString(doubleVal);
+            Console.WriteLine($"Explicit, -> string: \"{boolVal}\" + \"{doubleVal}\" -> " +
+                $"{stringResult}");
+            longResult = integerVal + Convert.ToInt64(stringVal);
+            Console.WriteLine($"Mixed, -> long: {integerVal} + {stringVal} ->" +
+                $"{longResult}");
+            Console.ReadKey();
+        }
+    }
+}
