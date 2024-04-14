f9f5acd3befc8b57d70db1dfdd60b09373ee4b4e
Mika <mika.juhani.vaisanen@outlook.com>
2024-04-14 16:22:37 +0300

    A console application that defines and uses a struct

---
+++ Chapter 5/StructExample/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace StructExample
+{
+    enum Orientation : byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4
+    }
+
+    struct Route
+    {
+        public Orientation direction;
+        public double distance;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Route myRoute;
+            int myDirection = -1;
+            double myDistance;
+            Console.WriteLine("1 North\n2) South\n3) East\n4) West");
+            do
+            {
+                Console.WriteLine("Select a direction:");
+                myDirection = Convert.ToInt32(Console.ReadLine());
+            }
+            while ((myDirection < 1) || (myDirection > 4));
+            Console.WriteLine("Input a distance:");
+            myDistance = Convert.ToDouble(Console.ReadLine());
+            myRoute.direction = (Orientation)myDirection;
+            myRoute.distance = myDistance;
+            Console.WriteLine($"myRoute specifies a direction of {myRoute.direction} " +
+                $"and a distance of {myRoute.distance}");
+            Console.ReadKey();
+        }
+    }
+}
