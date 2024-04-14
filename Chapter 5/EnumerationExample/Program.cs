9c39a61af6ef463e535ff71cb61e01808d6adfcd
Mika <mika.juhani.vaisanen@outlook.com>
2024-04-14 14:04:45 +0300

    A console application that defines and uses an enumeration type

---
+++ Chapter 5/EnumerationExample/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EnumerationExample
+{
+    internal class Program
+    {
+        enum Orientation : byte
+        {
+            north = 1,
+            south = 2,
+            east = 3,
+            west = 4
+        }
+
+        static void Main(string[] args)
+        {
+            byte directionByte;
+            string directionString;
+            Orientation myDirection = Orientation.north;
+            Console.WriteLine($"myDirection = {myDirection}");
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+            Console.WriteLine($"byte equivalent = {directionByte}");
+            Console.WriteLine($"string equivalent = {directionString}");
+            Console.ReadKey();
+        }
+    }
+}
