a09e9f44e331568a7ebfcf3c26f0ed13ecccf6b9
Mika <mika.juhani.vaisanen@outlook.com>
2024-04-14 18:14:55 +0300

    A console application that defines and uses a string array

---
+++ Chapter 5/ArrayExample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ArrayExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = { "Todd Anthony", "Mary Chris", "Autry Rual" };
+            int i;
+            Console.WriteLine($"Here are {friendNames.Length} of my friends:");
+            for (i = 0; i < friendNames.Length; i++)
+            {
+                Console.WriteLine(friendNames[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
